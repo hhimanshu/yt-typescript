25a6ac6499e982a7862b2e46a66c62325239bc07
hhimanshu <dev.atwork@yahoo.com>
2024-05-08 15:47:21 +1200

    chore: Add function to find minimum number in an array

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+const findMinimumNumber = (arr: number[]): number => {
+  let min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+
+const numbersFrom1To50UnOrdered = [50, 30, 20, 10, 40];
+const minimumNumber = findMinimumNumber(numbersFrom1To50UnOrdered);
+console.log(minimumNumber); // 10
+
